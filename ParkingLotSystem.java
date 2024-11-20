42cc476dbd7271ea695d82b6eb4aadd8f5e29e9b
ranjeet1996 <ranjeetsoni1996@gmail.com>
2024-11-20 19:29:12 +0530

    Initial commit

---
+++ ParkingLotSystem.java
@@ -0,0 +1,96 @@
+
+import java.util.HashMap;
+
+public class ParkingLotSystem {
+    public static void main(String[] args) {
+        HashMap<VehicleType, Floor> floorMapping = new HashMap<>();
+        Floor f1 = new Floor(100, 1);
+        floorMapping.put(VehicleType.CAR, f1);
+        floorMapping.put(VehicleType.BIKE, new Floor(150, 2));
+        floorMapping.put(VehicleType.TRUCK, new Floor(50, 3));
+
+
+        EntryGate m1 = new EntryGate("Chintu");
+        Vehicle v1 = new Vehicle("MZ12AA4455", VehicleType.CAR, "White", "Tesla", "X");
+        floorMapping.get(v1.vehicleType).display();
+        Ticket teslaTicket = m1.issueTicket(v1, floorMapping);
+        floorMapping.get(v1.vehicleType).display();
+        System.out.println("TicketNo:" + teslaTicket.ticketNo);
+        System.out.println("FloorNo:" + teslaTicket.floor.floorNo);
+        System.out.println("EntryTime:" + teslaTicket.entryTime);
+        Vehicle v2 = new Vehicle("MZ12AA4454", VehicleType.BIKE, "White", "Hero", "X");
+        floorMapping.get(v2.vehicleType).display();
+        Ticket Biketict = m1.issueTicket(v2, floorMapping);
+        floorMapping.get(v2.vehicleType).display();
+        System.out.println("TicketNo:" + Biketict.ticketNo);
+        System.out.println("FloorNo:" + Biketict.floor.floorNo);
+        System.out.println("EntryTime:" + Biketict.entryTime);
+    }
+}
+
+class Vehicle {
+    public Vehicle(String number, VehicleType vehicleType, String color, String brand, String model) {
+        this.number = number;
+        this.vehicleType = vehicleType;
+        this.color = color;
+        this.brand = brand;
+        this.model = model;
+    }
+
+    String number;
+    VehicleType vehicleType;
+    String color;
+    String brand;
+    String model;
+
+
+}
+
+class Gate {
+    String security;
+
+    Gate(String security) {
+        this.security = security;
+    }
+
+}
+
+class Ticket {
+    int ticketNo;
+    Floor floor;
+    String entryTime;
+
+    public Ticket(int ticketNo, Floor floor, String entryTime) {
+        this.ticketNo = ticketNo;
+        this.floor = floor;
+        this.entryTime = entryTime;
+        floor.currentlyOccupiedSlots++;
+    }
+
+}
+
+class EntryGate extends Gate {
+    int ticketNo = 1;
+
+    EntryGate(String security) {
+        super(security); // calling parent class constyructro
+    }
+
+
+    Ticket issueTicket(Vehicle v, HashMap<VehicleType, Floor> floorMapping) {
+        // returning an object from function
+        //find right floor for vehicle
+        Floor currentFloor = floorMapping.get(v.vehicleType);
+        if (currentFloor.canIPark() == false) {
+            System.out.println("Parking is full");
+            return null;
+        }
+        Ticket t1 = new Ticket(ticketNo, currentFloor, "10:30");
+        ticketNo++;
+        return t1;
+    }
+}
+
+enum VehicleType {
+    TRUCK, CAR, BIKE
+}
